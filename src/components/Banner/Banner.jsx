be7889e8ee392f7605644c15082a154ccc867944
maab-fw <maabfw@proton.me>
2024-05-11 01:40:06 +0600

    banner customized, data fetch done on popularService section

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -6,22 +6,22 @@ const Banner = () => {
     return (
         <div className="md:-ml-[8%] md:-mr-[8%] -ml-[5%] -mr-[5%] flex flex-col md:flex-row justify-between bg-base-100">
             <div className="md:w-1/2 flex flex-col justify-center items-center text-center md:text-left">
-                <h2 className="text-4xl font-bold w-2/3">
+                <h2 className="text-2xl md:text-4xl font-bold md:w-2/3">
                     Revive Your <span className="text-[#6366f1]">Electronics</span> with Our
                     <span className="text-[#6366f1]"> Expert</span> Repair Services!
                 </h2>
-                <p className="w-2/3 mt-5 text-gray-500">
+                <p className="w-[84%] md:w-2/3 mt-5 text-gray-500">
                     Trust our experienced technicians to restore your devices to full functionality. Serving both residential and
                     commercial clients, we offer fast turnaround times and competitive rates.
                 </p>
-                <div className="flex items-start w-2/3 mt-5">
+                <div className="flex items-start md:w-2/3 mt-5">
                     <Link to="/all-services" className="btn btn-info text-white">
                         All Services <FiArrowUpRight className="text-xl" />
                     </Link>
                 </div>
             </div>
             <img
-                className="md:w-1/2 h-[500px]"
+                className="md:w-1/2 h-[500px] object-contain"
                 src="https://cdn.dribbble.com/users/3737023/screenshots/19571641/media/6743a6a0dd318b67d732d6a23cb7ad78.gif"
                 alt=""
             />
--- src/components/PopularServices/PopularServices.jsx
+++ src/components/PopularServices/PopularServices.jsx
@@ -1,20 +1,47 @@
 import React from "react"
 import SingleServiceCard from "../SingleServiceCard/SingleServiceCard"
+import { useQuery } from "@tanstack/react-query"
+import axios from "axios"
+import Loading from "../Loading/Loading"
 
 const PopularServices = () => {
+    const { data, isPending, error, isError } = useQuery({
+        queryKey: ["services"],
+        queryFn: () =>
+            axios(`${import.meta.env.VITE_API_URL}/all-services`)
+                .then((res) => {
+                    // console.log(res.data)
+                    return res.data
+                })
+                .catch((e) => {
+                    console.log(e)
+                }),
+    })
+    // console.log(data)
+
+    if (isPending) return <Loading></Loading>
+
+    if (isError || error)
+        return (
+            <div className="grid place-content-center bg-white px-4">
+                <h1 className="uppercase tracking-widest text-gray-500">Something went wrong</h1>
+                <p>{error.message}</p>
+            </div>
+        )
+
     return (
-        <div className="my-12">
+        <div className="md:my-12">
             <div className="text-center ">
                 <h2 className="text-3xl font-bold">Popular Services</h2>
-                <p className="mt-3 w-2/3 mx-auto">
+                <p className="mt-3 w-[90%] md:w-2/3 mx-auto text-gray-500">
                     Explore our most sought-after offerings, designed to cater to your needs with precision and expertise. Benefit
                     from our commitment to quality, reliability, and customer satisfaction.
                 </p>
             </div>
             <div className="my-16 grid grid-cols-1 md:grid-cols-2 gap-6">
-                <SingleServiceCard></SingleServiceCard>
-                <SingleServiceCard></SingleServiceCard>
-                <SingleServiceCard></SingleServiceCard>
+                {data?.slice(0, 6).map((card) => (
+                    <SingleServiceCard key={card._id} card={card}></SingleServiceCard>
+                ))}
             </div>
         </div>
     )
