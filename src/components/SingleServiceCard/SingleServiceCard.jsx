d70c85cf74dca6f4b4982f7ff8fd20cfc8eac1df
maab-fw <maabfw@proton.me>
2024-05-11 08:57:06 +0600

    single service card design done

--- src/components/SingleServiceCard/SingleServiceCard.jsx
+++ src/components/SingleServiceCard/SingleServiceCard.jsx
@@ -1,16 +1,36 @@
 import React from "react"
+import { IoPricetags } from "react-icons/io5"
+import { Link } from "react-router-dom"
 
-const SingleServiceCard = () => {
+const SingleServiceCard = ({ card }) => {
+    const { _id, imageUrl, serviceName, price, description, providerImage, providerName } = card || {}
     return (
         <div className="card card-compact bg-base-100 shadow-xl">
-            <figure>
-                <img src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="Shoes" />
-            </figure>
             <div className="card-body">
-                <h2 className="card-title">Shoes!</h2>
-                <p>If a dog chews shoes whose shoes does he choose?</p>
+                <div className="flex gap-5 mb-2">
+                    <img className="size-14 border border-accent rounded-full" src={providerImage} alt="" />
+                    <div className="flex flex-col gap-2">
+                        <p className="font-semibold">{providerName}</p>
+                        <p className="text-gray-500">Service Provider</p>
+                    </div>
+                </div>
+                <figure className="mb-3">
+                    <img className="md:h-80 rounded object-cover w-full" src={imageUrl} />
+                </figure>
+
+                <div className="flex justify-between">
+                    <h2 className="card-title text-[#010030] text-2xl">{serviceName}</h2>
+                    <p className="flex items-center justify-end gap-3 text-lg text-success">
+                        <IoPricetags />
+                        {price}
+                    </p>
+                </div>
+                <p className="text-gray-700">{description.slice(0, 97)}...</p>
+
                 <div className="card-actions justify-end">
-                    <button className="btn btn-primary">Buy Now</button>
+                    <Link to={`/single-services/${_id}`} className="btn bg-[#6366f1] text-white">
+                        View Details
+                    </Link>
                 </div>
             </div>
         </div>
